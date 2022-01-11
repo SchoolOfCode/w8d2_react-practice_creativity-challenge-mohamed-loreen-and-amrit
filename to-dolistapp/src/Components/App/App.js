0efec14196540b483755673677fbf8f8c6783024
Tiger Fingers <loreen@anet360.com>
2022-01-11 15:05:56 +0000

    planning

---
+++ to-dolistapp/src/Components/App/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+//import an input
+//import a task behaviour?
+//import a state from react
+
+function App() {
+  //const []
+  //const []
+  //function event listener
+  //console.log the input
+
+  //function for when input submitted
+
+  //function for delete
+
+  //
+  return (
+    <div className="App">
+      <p>Hello Team</p>
+    </div>
+  );
+}
+
+export default App;
